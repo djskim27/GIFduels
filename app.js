a871763b3754e08e571e2f1a86c989269db4ba76
djskim27 <djs.kim27@gmail.com>
2017-07-27 17:03:07 -0400

    added mongo/mongoose

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI); 
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
