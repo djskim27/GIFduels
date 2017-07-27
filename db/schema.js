aa77017f98ea07af9de3b863f1ef74f9aa2b731d
djskim27 <djs.kim27@gmail.com>
2017-07-27 17:55:00 -0400

    renamed gifschema to schema and finished user and battle

--- db/schema.js
+++ db/schema.js
@@ -5,20 +5,88 @@ mongoose.Promise = global.Promise;
 //GIF Schema
 const gifSchema = new Schema({
     title: {
+
         type: String,
         required: true,
         unique: true
+
     },
     imgUrl: {
+
         type: String,
         required: true,
         unique: true
+
     },
     votes: {
+
         type: Number
+
     }
 });
 
-const Gifs = mongoose.model('Gifs', gifSchema)
+//User Schema
+const userSchema = new Schema({
+
+    firstName: {
+
+        type: String,
+        required: true
+
+    },
+    lastName: {
+
+        type: String,
+        required: true
+
+    },
+    email: {
+
+        type: String,
+        required: true,
+        unique: true,
+        
+    },
+    created_at: Date,
+    updated_at: Date,
+    gifs: [gifSchema]
+
+});
+
+//Battle Schema
+const battleSchema = new Schema({
+
+    userOne: {
+
+        type: String,
+        required: true
+
+    },
+    userTwo: {
+
+        type: String,
+        required: true
+
+    },
+    created_at: Date,
+    winner: {
+
+        type: String,
+        required: true,
+        
+    }
+
+});
+
+
+const Gif = mongoose.model('Gif', gifSchema);
+const User = mongoose.model('User', userSchema);
+const Battle = mongoose.model('Battle', battleSchema);
 //export GIFS with module.exports()
-module.exports = Gifs;
+module.exports = {
+
+    Gif,
+    User,
+    Battle
+
+}
