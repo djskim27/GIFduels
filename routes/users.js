08052fd4e11504b92648680dc50abe4c1455e4ef
JaceGarcia <jaces.place@gmail.com>
2017-07-28 11:19:32 -0400

    got server working

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
+router.get('/', (req, res) => {
+    User.find({}).then((users) => {
+      res.render(
+        'users/index',
+        { users },
+      );
+    }).catch((error) => {
+    console.log('Error retrieving users from database!');
+    console.log(error);
+    });
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
