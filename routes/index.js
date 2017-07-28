c02b55bcb7dd9944fa3f94587c065a0367b320c9
danremel <remel.dan@gmail.com>
2017-07-28 17:13:16 -0400

    Dan's leftover work. Objective, get users to pull into the battle

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const Battle = require('../models/battle');
+// const User = require('../models/user');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('homepage/index', { title: 'GIF DUELS' });
+router.get('/', (req, res) => {
+    Battle.find({}).then((battle) => {
+      res.render(
+        'homepage/index',
+        { 
+          // playerOne: User.playerOne,
+          // playerTwo: User.playerTwo
+          battle,
+          gifOne: battle.playerOne
+        }
+
+      );
+    }).catch((error) => {
+    console.log('Error retrieving users from database!');
+    console.log(error);
+    });
+          console.log(Battle.playerOne);
 });
 
 module.exports = router;
